50e220f9aebabd9b17ed19c1eea44dfa673b6bd8
Luca Nguyen <108933241+smoluca123@users.noreply.github.com>
2023-09-08 17:47:34 +0700

    cau 4 5 6 7

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,6 +40,70 @@ function timSoNhoNhat(arr) {
   return smallest;
 }
 
+// Tìm số dương nhỏ nhất
+function timSoDuongNhoNhat(arr) {
+  if (!checkEmptyArr(arr)) {
+    return;
+  }
+  var listSoDuong = demSoDuong(arr);
+  var smallest = listSoDuong[0];
+  for (var item of listSoDuong) {
+    if (item < smallest) {
+      smallest = item;
+    }
+  }
+  return smallest;
+}
+
+// tìm số chẵn cuối cùng
+function timSoChanCuoiCung(arr) {
+  if (!checkEmptyArr(arr)) {
+    return;
+  }
+  var soChanCuoiCung = arr[0];
+  for (var soChan of arr) {
+    if (soChan % 2 == 0) {
+      soChanCuoiCung = soChan;
+    }
+  }
+  return soChanCuoiCung;
+}
+
+// đổi vị trí trong mảng
+
+function swapItemArr(arr, i, j) {
+  if (!checkEmptyArr(arr)) {
+    return;
+  }
+  var arrClone = [...arr];
+  var output = {
+    item1: arrClone[i],
+    item2: arrClone[j],
+  };
+  arrClone[i] = arrClone[i] + arrClone[j];
+  arrClone[j] = arrClone[i] - arrClone[j];
+  arrClone[i] = arrClone[i] - arrClone[j];
+  output.arr = arrClone;
+  return output;
+}
+
+function sapXepTang(arr) {
+  if (!checkEmptyArr(arr)) {
+    return;
+  }
+  var arrSort = [...arr];
+  for (var i = 0; i < arrSort.length; i++) {
+    for (var j = i; j < arrSort.length; j++) {
+      if (arrSort[i] > arrSort[j]) {
+        arrSort[i] = arrSort[i] + arrSort[j];
+        arrSort[j] = arrSort[i] - arrSort[j];
+        arrSort[i] = arrSort[i] - arrSort[j];
+      }
+    }
+  }
+  return { arr, arrSort };
+}
+
 function checkEmptyArr(arr) {
   if (arr.length <= 0) {
     return false;
@@ -59,19 +123,23 @@ function App() {
     var inputNum = $a('.form-input input');
     // push giá trị vào mảng arrNum
     arrNum.push(+inputNum.value);
+    inputNum.value = '';
     // render ra giao diện
     renderResult(notis[0], `[${arrNum}]`);
   };
+
+  // cau 1
   btns[0].onclick = function () {
     var result = tinhTongSoDuong(arrNum);
-    if (result) {
-      console.log(result);
+    if (result || result == 0) {
       renderResult(notis[1], `Tổng các số dương là : ${result}`);
     }
   };
+
+  //   cau 2
   btns[1].onclick = function () {
     var result = demSoDuong(arrNum);
-    if (result) {
+    if (result || result == 0) {
       renderResult(
         notis[2],
         `Có ${result.length} số dương trong mảng, là: ${result}`
@@ -79,10 +147,51 @@ function App() {
     }
   };
 
+  //   cau 3
   btns[2].onclick = function () {
     var result = timSoNhoNhat(arrNum);
-    if (result) {
+    if (result || result == 0) {
       renderResult(notis[3], `Số nhỏ nhất trong mảng là : ${result}`);
+    }
+  };
+
+  //cau 4
+  btns[3].onclick = function () {
+    var result = timSoDuongNhoNhat(arrNum);
+    if (result || result == 0) {
+      renderResult(notis[4], `Số dương nhỏ nhất trong mảng là : ${result}`);
+    }
+  };
+
+  //cau 5
+  btns[4].onclick = function () {
+    var result = timSoChanCuoiCung(arrNum);
+    if (result || result == 0) {
+      renderResult(notis[5], `Số chẵn cuối cùng trong mảng là : ${result}`);
+    }
+  };
+
+  //cau 6
+  btns[5].onclick = function () {
+    var inputs = $all('#collapseSix input');
+    var result = swapItemArr(arrNum, +inputs[0].value, +inputs[1].value);
+    if (result || result == 0) {
+      renderResult(
+        notis[6],
+        `Đã đổi vị trí 2 phần tử ${result.item1} và ${result.item2}\nMảng sau khi thay đổi là : ${result.arr}`
+      );
+    }
+  };
+
+  //cau 7
+  btns[6].onclick = function () {
+    var result = sapXepTang(arrNum);
+
+    if (result || result == 0) {
+      renderResult(
+        notis[7],
+        `Mảng trước : ${result.arr} <br>👉 và sau khi đã sắp xếp : ${result.arrSort}`
+      );
     }
   };
 }
